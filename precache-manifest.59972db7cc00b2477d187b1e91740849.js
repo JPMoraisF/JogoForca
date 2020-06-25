7eef5d4366006dc4c09925fc5f548d05d1e3a1b4
João Pedro Morais <jpmferreira96@gmail.com>
2020-06-24 23:23:14 -0300

    Updates

---
+++ precache-manifest.59972db7cc00b2477d187b1e91740849.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389a0021ea7756fc6c615e6a3f2db661",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe3e57e11333548c7d97",
+    "url": "/static/css/main.cfbbe64f.chunk.css"
+  },
+  {
+    "revision": "8949c08f4be36e454c2b",
+    "url": "/static/js/2.c94a1643.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c94a1643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe3e57e11333548c7d97",
+    "url": "/static/js/main.cf617f79.chunk.js"
+  },
+  {
+    "revision": "3fab50f78bcfdcb57e06",
+    "url": "/static/js/runtime-main.f49d6ff0.js"
+  }
+]);
